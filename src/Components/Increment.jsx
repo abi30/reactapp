47b0991627f1379da49d4417e37137b4944f6a5c
abi30 <rakib0751@gmail.com>
2022-09-18 23:49:27 +0200

    test goin

---
+++ src/Components/Increment.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+
+function Increment() {
+  const [counter, setCounter] = React.useState(0);
+  console.log(counter);
+  console.log(setCounter);
+
+  return (
+    <div>
+      <h1 id="display">{counter}</h1>
+      <div>
+        <Button id="button" onClick={()=>setCounter(counter + 1)}>
+          counter + 1
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Increment;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from './logo.svg';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
@@ -10,13 +9,17 @@ import './index.css';
 
 
 
-
-
 // document.getElementById('root').append(JSON.stringify(page));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <App/>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <App/>
-  
+
 );
 
 
