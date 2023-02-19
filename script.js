2ae782c84fcf5bbf265a80b70b6b0273df0f22f5
Abdulla al Rakib <abdullaalrakib@Abdullas-MacBook-Pro.local>
2023-02-19 23:05:32 +0100

    test 1.16

--- script.js
+++ script.js
@@ -3,22 +3,43 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+//actions creators
+const increment =(value)=>{
+    return {
+        type:INCREMENT,
+        payload:value
+    }
+
+};
+const decrement = (value)=>{
+    return {
+        type:DECREMENT,
+        payload:value
+    }
+
+};
+
+
 // initial state
 const initialState = {
     value: 0,
 };
+
 
 // create reducer function
 function counterReducer(state = initialState, action) {
     if (action.type === "increment") {
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         };
     } else if (action.type === "decrement") {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         };
     } else {
         return state;
@@ -38,15 +59,13 @@ render();
 
 store.subscribe(render);
 
+
+
 // button click listeners
 incrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment",
-    });
+    store.dispatch(increment(2));
 });
 
 decrementEl.addEventListener("click", () => {
-    store.dispatch({
-        type: "decrement",
-    });
+    store.dispatch(decrement(1));
 });
