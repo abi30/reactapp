b285cd02bdf3e3768015a8d6dc1454d7c1d28dde
abi30 <rakib0751@gmail.com>
2023-03-07 15:36:40 +0100

    test test

---
+++ rtk/app/store.js
@@ -0,0 +1,13 @@
+const configureStore = require("@reduxjs/toolkit").configureStore;
+
+const counterReducer = require("../features/counter/counterSlice");
+
+
+// configure Store 
+const store = configureStore({
+    reducer:{
+        counter:counterReducer,
+    }
+});
+
+module.exports = store;
---
+++ rtk/features/counter/counterSlice.js
@@ -0,0 +1,23 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+
+// initial state
+const initialState ={
+    count:0,
+};
+
+const counterSlice = createSlice({
+    name:"counter",
+    initialState,
+    redeucers : {
+        increment : (state,action) =>{
+            state.count++;
+        },
+        decrement : (state,action) =>{
+            state.count--;
+        },
+    },
+});
+module.exports = counterSlice.reducer;
+module.exports.counterActions = counterSlice.actions;
+
+
